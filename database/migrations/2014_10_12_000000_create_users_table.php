652a8404033ef1142b7177581597a4a49116d978
apellegero <albertpeye@gmail.com>
2017-04-09 14:41:18 +0200

    Data Base creada y migraciones finalizadas

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('nusuario');
+            $table->string('email')->unique;
             $table->string('password');
+            $table->string('nombre');
+            $table->string('telefono');
+            $table->integer('tipo_id');
             $table->rememberToken();
             $table->timestamps();
         });

0aff3b8a496fcf2dd5445e3cc74150f65372c77a
apellegero <albertpeye@gmail.com>
2017-04-16 18:29:59 +0200

    foto acabado 2

---
+++ app/Http/Controllers/UploadController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\User;
+use App\Cliente;
+use App\Tienda;
+use \Input as Input;
+use Image;
+
+class UploadController extends Controller{
+	public function upload(Request $request){
+		if($request->hasFile('foto')){
+			$foto = $request->file('foto');
+			$filename = time().'.'.$foto->getClientOriginalExtension();
+			Image::make($foto)->resize(300, 300)->save(public_path('uploads/avatars/' .$filename));
+
+			$user = Auth::user();
+			$user->foto = $filename;
+			$user->save();
+
+			if(isset($request['from'])){
+				return view($request['from']);
+			}
+			else{
+				return view('/');//no funciona
+			}
+		}
+	}
+}

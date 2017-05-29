11a31d19860eea4fa88b9347e32b8238ba60156b
xavierGimenezz <xavier.a.gimenez@gmail.com>
2017-05-29 12:20:22 +0200

    registro fantasma arreglado y testeado

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -23,66 +23,72 @@ class UserController extends Controller{
 			'nusuario' => 'required|unique:users',
 			'password' => 'required|min:4',
             'telefono' => 'min:9|integer'
+
 		]);
-		//Recollim les dades directament del formulari
-		$email = $req['email'];
-		$nusuario = $req['nusuario'];
-		$password = bcrypt($req['password']);
-		$telefono = $req['telefono'];
-		$nombre = $req['nombre'];
-		$tipo = $req['tipo'];
+		if($this->fails()){
 
-		//Creem l'objecte User i les desem
-		$user = new User();
-		$user->email = $email;
-		$user->nusuario = $nusuario;
-		$user->password = $password;
-		$user->telefono = $telefono;
-		$user->nombre = $nombre;
-		$user->tipo_id = $tipo;
-		$user->save();
+        }
+        else{
+            //Recollim les dades directament del formulari
+            $email = $req['email'];
+            $nusuario = $req['nusuario'];
+            $password = bcrypt($req['password']);
+            $telefono = $req['telefono'];
+            $nombre = $req['nombre'];
+            $tipo = $req['tipo'];
 
-		//direccion
-		if($tipo!=3){
-			$direccion = new Direccion();
-			$direccion->id_usuario = $user->id;
-			$direccion->id_pais = 0;
-			$direccion->id_poblacion = 0;
-			$direccion->id_distrito = 0;
-			$direccion->cp = $req['cp'];
-			$direccion->calle = $req['calle'];
-			$direccion->numero_calle = $req['numero_calle'];
-			$direccion->piso = $req['piso'];
-			$direccion->sugerencias = $req['sugerencias'];
-			$direccion->save();
-		}
+            //Creem l'objecte User i les desem
+            $user = new User();
+            $user->email = $email;
+            $user->nusuario = $nusuario;
+            $user->password = $password;
+            $user->telefono = $telefono;
+            $user->nombre = $nombre;
+            $user->tipo_id = $tipo;
+            $user->save();
 
-		//Com User pot ser Client, Repartidor, o Botiga necessitem crear els altres objectes també
-		if($tipo==1){
-			$cliente = new Cliente();
-			$cliente->id_user = $user->id;
-			$cliente->apellido = $req['apellido'];
-			$cliente->fecha_nacimiento = $req['fecha_nacimiento'];
-			$cliente->save();
+            //direccion
+            if($tipo!=3){
+                $direccion = new Direccion();
+                $direccion->id_usuario = $user->id;
+                $direccion->id_pais = 0;
+                $direccion->id_poblacion = 0;
+                $direccion->id_distrito = 0;
+                $direccion->cp = $req['cp'];
+                $direccion->calle = $req['calle'];
+                $direccion->numero_calle = $req['numero_calle'];
+                $direccion->piso = $req['piso'];
+                $direccion->sugerencias = $req['sugerencias'];
+                $direccion->save();
+            }
+
+            //Com User pot ser Client, Repartidor, o Botiga necessitem crear els altres objectes també
+            if($tipo==1){
+                $cliente = new Cliente();
+                $cliente->id_user = $user->id;
+                $cliente->apellido = $req['apellido'];
+                $cliente->fecha_nacimiento = $req['fecha_nacimiento'];
+                $cliente->save();
+            }
+            if($tipo==2){
+            //Tienda
+                $tienda = new Tienda();
+                $tienda->id_user = $user->id;
+                $tienda->nie = $req['nie'];
+                $tienda->save();
+            }
+            if($tipo==3){
+            //Repartidor
+                $repartidor = new Repartidor();
+                $repartidor->id_user = $user->id;
+                $repartidor->id_tienda = Auth::user()->id;
+                $repartidor->apellido = $req['apellido'];
+                $repartidor->save();
+                return redirect()->route('gestorrepartidores');
+            }
+            Auth::login($user);
+            return redirect()->route('pagprincipal');
 		}
-		if($tipo==2){
-		//Tienda
-			$tienda = new Tienda();
-			$tienda->id_user = $user->id;
-			$tienda->nie = $req['nie'];
-			$tienda->save();
-		}
-		if($tipo==3){
-		//Repartidor
-			$repartidor = new Repartidor();
-			$repartidor->id_user = $user->id;
-			$repartidor->id_tienda = Auth::user()->id;
-			$repartidor->apellido = $req['apellido'];
-			$repartidor->save();
-			return redirect()->route('gestorrepartidores');
-		}
-		Auth::login($user);
-		return redirect()->route('pagprincipal');
 	}
 
 	public function singIn(Request $req){
